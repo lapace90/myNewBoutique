5f8ed34b2768282bd41a645abd6a4b0c34460a23
Ilaria <lapace90@gmail.com>
2024-04-11 09:09:35 +0200

    modif order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\Order;
+use App\Services\Cart;
 use App\Form\OrderType;
+use App\Entity\OrderDetails;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+//TODO: verifier TOUS les commentaires
 class OrderController extends AbstractController
 {
     #[Route('/order', name: 'order')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
+    public function index(Request $request, EntityManagerInterface $manager, Cart $cart, ProductRepository $repo): Response
     {
         // if (!$this->getUser()->getAddresses()->getValues())
         //     {
@@ -21,8 +28,44 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user'=>$this->getUser()
         ]);
+        // $cart = $cart->get();
+        $cartComplete = [];
+        // dd($cart);
+        foreach ($cart as $id => $quantity) {
+        $cartComplete[] = [
+        'product' => $repo->findOneById($id),
+        'quantity' => $quantity,
+        ];
+        }
         $form->handleRequest($request);
         if( $form->isSubmitted() && $form->isValid()){
+            $order = new Order();
+            $order->setUser($this->getUser())
+            //->setCreatedAt(new DateTime())
+            ->setCarrier ($form->get('transporteurs')-> getData())
+            ->setDelivery($form->get('addresses')-> getData())
+            ->setStatut(0);
+
+            $date = new \DateTime();
+            $date = $date->format('dmY');
+            $order->setReference($date . '-' . uniqid());
+            $manager->persist($order);
+            // Enregistrer mes produit OrderDetails
+            //dump($cartComplete);
+            foreach ($cartComplete as $product) {
+            $orderDetails = new OrderDetails();
+            $orderDetails->setMyOrder($order);
+            $orderDetails->setProduct($product['product']);
+            $orderDetails->setQuantity($product['quantity']);
+            $orderDetails->setPrice($product['product']->getPrice());
+            //dump($product);
+            $manager->persist($orderDetails);
+            }
+            //$manager->flush();
+            // return $this->render('order/recap.html.twig', [
+            // 'cart' => $cartComplete,
+            // 'order' => $order,
+            // ]);
 
         }
         
